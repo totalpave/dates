d780778e623e66d5f34c468c3ca2e94d9dc53028
Norman Breau <norman@normanbreau.com>
2020-12-19 14:17:02 -0400

    deproll

--- spec/DateUtils.spec.ts
+++ spec/DateUtils.spec.ts
@@ -4,28 +4,28 @@ import {DateUtils} from '../src/DateUtils';
 describe('DateUtils', () => {
     describe('getYearDifference', () => {
         it('2018-2019', () => {
-            const start: Date = new Date(2018, 1, 1);
-            const end: Date = new Date(2019, 1, 1);
+            let start: Date = new Date(2018, 1, 1);
+            let end: Date = new Date(2019, 1, 1);
     
             expect(DateUtils.getYearDifference(start, end)).toBe(1);
         });
     
         it('2017-05-3 - 2019-06-26', () => {
-            const start: Date = new Date(2017, 4, 3);
-            const end: Date = new Date(2019, 5, 26);
+            let start: Date = new Date(2017, 4, 3);
+            let end: Date = new Date(2019, 5, 26);
     
             expect(DateUtils.getYearDifference(start, end, true)).toBe(2.15);
         });
     
         it('1995-06-01 -> 2016-08-12', () => {
-            const start: Date = new Date(1995, 5, 1);
-            const end: Date = new Date(2016, 7, 12);
+            let start: Date = new Date(1995, 5, 1);
+            let end: Date = new Date(2016, 7, 12);
             expect(DateUtils.getYearDifference(start, end, true))
         });
     });
 
     describe('Adding/Subtracting Year Diffs', () => {
-        const date: Date = new Date(2018, 1, 1);
+        let date: Date = new Date(2018, 1, 1);
 
         it('add', () => {
             expect(DateUtils.addYearDifference(date, 1).getFullYear()).toBe(2019);
@@ -37,7 +37,7 @@ describe('DateUtils', () => {
     });
 
     describe('getStartOfYearDate', () => {
-        const year: number = 2015;
+        let year: number = 2015;
 
         it('returns date with correct year', () => {
             expect(DateUtils.getStartOfYearDate(year).getFullYear()).toBe(year);
@@ -54,7 +54,7 @@ describe('DateUtils', () => {
     });
 
     describe('getEndOfYearDate', () => {
-        const year: number = 2015;
+        let year: number = 2015;
 
         it('returns date with correct year', () => {
             expect(DateUtils.getEndOfYearDate(year).getFullYear()).toBe(year);
